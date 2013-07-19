dfda43dec3ba67cc5b050695c32e08de88e2d445
rae jin <rae.jin@gmail.com>
2013-07-19 07:00:51 +0000

    Edited web.js for index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var mystring = Buffer (30);
+  mystring = fs.readFileSync('index.html');
+  response.send(mystring.toString());
 });
 
 var port = process.env.PORT || 5000;
