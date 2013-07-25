ec691e80eb08625eacf4bfc5213780b214321d42
rae jin <rae.jin@gmail.com>
2013-07-25 08:22:10 +0000

    change web.js for static link

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname + '/static'));
 app.get('/', function(request, response) {
   var mystring = Buffer (30);
   mystring = fs.readFileSync('index.html');
   response.send(mystring.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
