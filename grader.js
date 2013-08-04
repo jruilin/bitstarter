eefb587cecda26553895e70e03af5a5ec82c0662
rae jin <rae.jin@gmail.com>
2013-08-04 06:20:35 +0000

    Edited product desc in index.html

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var LINK_DEFAULT = "http://frozen-escarpment-6714.herokuapp.com/";
+ 
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,11 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+ 
+var assertLinkExists =function(infile) {
+    var instr = infile.toString();
+    return instr;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,7 +72,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <url_file', 'Link to url', clone(assertLinkExists), LINK_DEFAULT) 
+    .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
